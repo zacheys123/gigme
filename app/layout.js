6fcd21d10eace68d0d20ebdeb4cbb8cb833b648b
zacharia muigai <user.zacheyshop@gmail.com>
2024-04-16 06:34:06 -0700

    co

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className="min-h-screen bg-gray-100 ">{children}</body>
+        <body className="min-h-screen bg-gray-200 ">{children}</body>
       </html>
     </ClerkProvider>
   );
