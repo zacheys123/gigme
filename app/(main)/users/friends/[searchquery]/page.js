84e1d951eefea52fc6a4945f7d89e3125fc227b8
zacharia muigai <user.zacheyshop@gmail.com>
2024-04-18 14:32:27 +0300

    created a dynamic search friend route

---
+++ app/(main)/users/friends/[searchquery]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FriendsPage = () => {
+  return <div>A friend is Found Here</div>;
+};
+
+export default FriendsPage;
--- app/gigme/(social)/search/page.js
+++ app/gigme/(social)/search/page.js
@@ -1,15 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 import { Search, SearchIcon } from "lucide-react";
 import { TextInput, Label } from "flowbite-react";
 import { Avatar } from "@mui/material";
+import { useRouter } from "next/navigation";
 const SearchPage = () => {
+  const [searchquery, setSearchhQuery] = useState("");
+  const router = useRouter();
   return (
     <div className="relative bg-slate-700 w-[100vw] h-[100vh]">
       <form className=" w-[100vw] ">
         <div className=" flex justify-center items-center">
           <Input
+            onChange={(ev) => setSearchhQuery(ev.target.value)}
+            value={searchquery}
             className="w-[100vw] mx-4 my-10 bg-black text-neutral-300"
             id="search"
             type="text"
@@ -19,7 +25,10 @@ const SearchPage = () => {
         </div>
       </form>
 
-      <div className=" bg-gray-800/80 ml-[40px] text-neutral-400 w-[300px] rounded-xl p-2">
+      <div
+        onClick={() => router.push(`/users/friends/${searchquery}`)}
+        className=" bg-gray-800/80 ml-[40px] text-neutral-400 w-[300px] rounded-xl p-2 cursor-pointer"
+      >
         <div className="flex gap-4 items-center ">
           {" "}
           <Avatar className="rounded-full" />
