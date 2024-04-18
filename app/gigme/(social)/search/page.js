3b77fdb0b12da60b3c809d73db7613cf1e2eabd1
zacharia muigai <user.zacheyshop@gmail.com>
2024-04-18 14:22:33 +0300

    added a user display

--- app/gigme/(social)/search/page.js
+++ app/gigme/(social)/search/page.js
@@ -3,10 +3,11 @@ import { Input } from "@/components/ui/input";
 
 import { Search, SearchIcon } from "lucide-react";
 import { TextInput, Label } from "flowbite-react";
+import { Avatar } from "@mui/material";
 const SearchPage = () => {
   return (
     <div className="relative bg-slate-700 w-[100vw] h-[100vh]">
-      <form className="absolute w-[100vw] ">
+      <form className=" w-[100vw] ">
         <div className=" flex justify-center items-center">
           <Input
             className="w-[100vw] mx-4 my-10 bg-black text-neutral-300"
@@ -17,6 +18,17 @@ const SearchPage = () => {
           />
         </div>
       </form>
+
+      <div className=" bg-gray-100/80 ml-[40px] w-[300px] rounded-xl p-3">
+        <div className="flex gap-2">
+          {" "}
+          <Avatar className="rounded-full" />
+          <div>
+            <div className="flex flex-col gap-2">Zachari Muigai</div>
+            <div>zachyb92@gmail.com</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
