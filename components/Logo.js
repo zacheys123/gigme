fcbb577eae6baab2e34178f543c1636e95bcdeb4
zacharia muigai <zacheyshop@gmail.com>
2024-09-26 01:50:01 +0300

    implement new logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,22 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import logo from "../public/assets/png/logo-white.png";
+
+import logo2 from "../public/assets/logo/png/logo-no-background.png";
 const Logo = ({ flex }) => {
   return (
     <Link href="/gigme/social" className={` flex  hover:cursor-pointer`}>
       <Image
-        src={logo}
+        src={logo2}
         width={100}
         height={50}
         alt="logo"
-        className="w-[100px] h-[60px] hidden md:flex"
+        className="w-[60px] h-[12px]  md:w-[80px] md:h-[20px]  lg:w-[80px] lg:h-[20px] xl:w-[80px] xl:h-[20px]"
       />
-      <div className="w-[60px] h-[30px] px-4 py-1 bg-gray-400 md:hidden rounded-ee-xl rounded-tr-lg">
-        <span className=" text-purple-700 font-bold font-mono  rounded-b-xl  text-[18px]  md:text-[14px]">
-          G
-        </span>
-      </div>
     </Link>
   );
 };
