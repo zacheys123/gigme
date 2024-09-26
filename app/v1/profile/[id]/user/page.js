240c6ea06a385c175d44690c3504ed58411cdbe3
zacharia muigai <zacheyshop@gmail.com>
2024-09-26 22:54:36 +0300

    all new commit

--- app/v1/profile/[id]/user/page.js
+++ app/v1/profile/[id]/user/page.js
@@ -160,7 +160,13 @@ const UserProfile = () => {
       ease: "easeInOut",
     },
   };
-
+  if (!user) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <h6 className="text-white">Loading...</h6>
+      </div>
+    );
+  }
   const foll =
     user?.followers?.length === 1
       ? `${user?.followers?.length} follower`
