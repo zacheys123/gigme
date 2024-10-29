3e0c7dd04fd4758d28be35372e77df8f747ad406
zacharia muigai <zacheyshop@gmail.com>
2024-10-29 16:58:53 +0300

    finally deployed server,first test

--- app/server-actions/getAllUsers.js
+++ app/server-actions/getAllUsers.js
@@ -11,8 +11,30 @@ export async function getAllUsers(id) {
 
     const users = await User.find({ _id: { $ne: id } });
 
-    return users;
+    // Create response with no-store cache
+    const response = NextResponse.json(users, { status: 200 });
+    response.headers.set("Cache-Control", "no-store");
+
+    return response;
   } catch (error) {
     return NextResponse.json({ message: error }, { status: 500 });
   }
 }
+
+// export async function GET(request) {
+//   try {
+//     await connectDb();
+
+//     const { searchParams } = new URL(request.url);
+//     const id = searchParams.get("id");
+
+//     const users = await User.find({ _id: { $ne: id } });
+
+//     const response = NextResponse.json(users, { status: 200 });
+//     response.headers.set("Cache-Control", "no-store");
+
+//     return response;
+//   } catch (error) {
+//     return NextResponse.json({ message: error }, { status: 500 });
+//   }
+// }
